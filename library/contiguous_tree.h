897dd930e4ce7a6f3bdb6a8aa18646090feb1dc5
Sergii Zaiets <sergii@flightkeys.com>
2020-06-02 16:22:41 +0200

    WIP

--- library/contiguous_tree.h
+++ library/contiguous_tree.h
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
 
-class Contiguous_tree {
-  public:
+template <class T>
+class Contiguous_node
+{
+  T data_;
+  size_t children_begin_;
+  size_t children_end_;
+  std::vector<Contiguous_node> *nodes_;
+  bool owns_; // TODO: can be removed
 
+  Contiguous_node(size_t children_begin,
+                  size_t children_end,
+                  std::vector<T> *nodes_,
+                  bool owns)
+  {
+    // on init children_begin = children_begin = index
+  }
+
+public:
+  ~Contiguous_node()
+  {
+    if (owns_)
+    {
+      delete nodes_;
+    }
+  }
+
+  Contiguous_node(T data)
+  {
+    nodes_ = new std::vector<T>();
+    nodes_->push_back(data);
+    owns_ = true;
+    children_begin = children_begin = 0;
+  }
+
+  T &data() { return data_; }
+
+  Contiguous_node add_child(T data) { children_.push_back(T); }
+
+  template std::vector<T>::const_iterator children_begin() const;
+  template std::vector<T>::const_iterator children_end() const;
 };
--- library/tree.h
+++ library/tree.h
@@ -1,19 +1,36 @@
 #include <vector>
 
+// template <class T>
+// class Tree
+// {
+// public:
+//   class Node
+//   {
+//   public:
+//     T value_;
+//     std::vector<Node> nodes_;
+//     Node(T value) : value_(value) {}
+//   };
+
+//   Tree(T value) : root_(value) {}
+
+// private:
+//   Node root_;
+// };
+
+/*
+ *
+ */
+
 template <class T>
-class Tree
+class Node
 {
+  T data_;
+  std::vector<T> children_;
+
 public:
-  class Node
-  {
-  public:
-    T value_;
-    std::vector<Node> nodes_;
-    Node(T value) : value_(value) {}
-  };
-
-  Tree(T value) : root_(value) {}
-
-private:
-  Node root_;
+  Node(T data) : data_(std::move(data)) {}
+  T &data() { return data_; }
+  void add_child(T data) { children_.push_back(T); }
+  std::vector<T> const &children() const { return children_; }
 };
