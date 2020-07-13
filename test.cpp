232d7adbf4fbdb4d3900a987237134015e7615e9
Sergii Zaiets <sergii.zaiets@gmail.com>
2020-07-13 22:26:48 +0200

    WIP

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(Test, test_1) {
+  EXPECT_TRUE(true);
+}
+
+TEST(Test1, test_2) {
+  EXPECT_TRUE(true);
+}
+
+TEST(Test2, test_3) {
+  EXPECT_TRUE(true);
+}
+
+TEST(Test3, test_4) {
+  EXPECT_TRUE(true);
+}
+
+TEST(Test4, test_5) {
+  EXPECT_TRUE(true);
+}
