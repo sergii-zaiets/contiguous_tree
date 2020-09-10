3b5ceac3f173c3a6a1789b408f66339830c31b00
Sergii Zaiets <sergii@flightkeys.com>
2020-09-10 13:42:49 +0200

    WIP

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -23,8 +23,7 @@ BENCHMARK(bm_create_classical_tree)
     ->Args({12, 2})
     ->Args({13, 3})
     ->Args({13, 4})
-    ->Args({27, 2})
-    ->Args({28, 2});
+    ->Args({27, 2});
 
 // Define another benchmark
 static void bm_create_contiguous_tree(benchmark::State &state) {
@@ -46,5 +45,27 @@ BENCHMARK(bm_create_contiguous_tree)
     ->Args({12, 2})
     ->Args({13, 3})
     ->Args({13, 4})
-    ->Args({27, 2})
-    ->Args({28, 2});
+    ->Args({27, 2});
+
+
+// Define another benchmark
+static void bm_create_contiguous_tree_in_width(benchmark::State &state) {
+  int depth = state.range(0);
+  int width = state.range(1);
+  for (auto _ : state) {
+    contiguous::Tree<int> tree =
+        create_contiguous_tree_in_width<int>(depth, width, []() {
+          static int i = 0;
+          return ++i;
+        });
+    ASSERT_FALSE(tree.empty());
+  }
+}
+BENCHMARK(bm_create_contiguous_tree_in_width)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({10, 2})
+    ->Args({11, 2})
+    ->Args({12, 2})
+    ->Args({13, 3})
+    ->Args({13, 4})
+    ->Args({27, 2});
